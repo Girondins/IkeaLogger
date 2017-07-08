aa1a0dfbf3916d678a9ae5d0721e631c50fed733
Girondins <stmsfsd@gmail.com>
2017-07-08 14:17:13 +0200

    pi

--- app/src/main/java/com/example/girondins/ikealogger/Database.java
+++
@@ -1,8 +0,0 @@
-package com.example.girondins.ikealogger;
-
-/**
- * Created by Girondins on 18/06/17.
- */
-
-public class Database {
-}
--- app/src/main/java/com/example/girondins/ikealogger/MainActivity.java
+++ app/src/main/java/com/example/girondins/ikealogger/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.girondins.ikealogger;
 
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,18 +14,21 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.TimeZone;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView dateText,taskText,fullText;
+public class MainActivity extends Activity {
+    private TextView dateText,taskText,fullText,timeText;
     private Button dateBtn,taskBtn,startBtn,stopBtn;
     private boolean started = false;
-    private String date,time,selectedTask,checkDate,startTime;
+    private String date,time,selectedTask,checkDate,startTime,startedTask;
     private String[] ikeaKod = {"Kassa -> 6180", "Sälj/X-Kassa -> 3300", "Obokad/X-Kassa -> 6122"};
     private AlertDialog.Builder alertDialogBuilder, datePicker;
-    private int kass,sälj,obok;
+    private int kass=0,sälj=0,obok=0;
+    private boolean resumeHasRun = false;
 
 
     @Override
@@ -33,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
         initiateComp();
         getDate();
         getTime();
+        showWorkedTime();
+        selectedTask = ikeaKod[0];
+        startedTask = selectedTask;
         dateText.setText(date);
         taskText.setText(ikeaKod[0]);
         stopBtn.setClickable(started);
@@ -64,6 +72,8 @@ public class MainActivity extends AppCompatActivity {
                 stopBtn.setClickable(started);
                 getTime();
                 startTime = time;
+                startedTask = selectedTask;
+                System.out.println(startedTask);
                 fullText.setText(fullText.getText() + "" + taskText.getText() + " \nStart: " + time);
             }
         });
@@ -71,13 +81,37 @@ public class MainActivity extends AppCompatActivity {
         stopBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+                int minutes = 0;
                 started = false;
                 startBtn.setClickable(!started);
                 stopBtn.setClickable(started);
                 getTime();
+                try {
+                    Date start = format.parse(startTime);
+                    Date end = format.parse(time);
+                    long diff = end.getTime() - start.getTime();
+                    minutes = (int) diff/(60*1000);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
                 fullText.setText(fullText.getText() + " End: " + time + "\n" + "\n");
+
+                switch(startedTask){
+                    case "Kassa -> 6180":
+                        kass = kass + minutes;
+                        break;
+                    case "Sälj/X-Kassa -> 3300":
+                        sälj = sälj + minutes;
+                        break;
+                    case "Obokad/X-Kassa -> 6122":
+                        obok = obok + minutes;
+                        break;
+                }
+                showWorkedTime();
             }
         });
+
 
     }
 
@@ -89,8 +123,15 @@ public class MainActivity extends AppCompatActivity {
         taskBtn = (Button) findViewById(R.id.taskBtnID);
         startBtn = (Button)findViewById(R.id.startBtnID);
         stopBtn = (Button) findViewById(R.id.stopBtnID);
+        timeText = (TextView) findViewById(R.id.toTimeID);
         fullText.setMovementMethod(new ScrollingMovementMethod());
 
+    }
+
+    public void showWorkedTime(){
+        timeText.setText("Kassa: " + kass +"\n"
+                        + "Sälj: " + sälj +"\n"
+                        + "Obok: " + obok);
     }
 
     public void getDate(){
@@ -168,5 +209,48 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences sp = getApplication().getSharedPreferences("SaveState", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.clear();
+        editor.putString("selectedTask",selectedTask);
+        editor.putString("startedTask",startedTask);
+        System.out.println(selectedTask);
+        editor.putString("startTime",startTime);
+        editor.putBoolean("started",started);
+        editor.putString("fullText",fullText.getText().toString());
+        editor.putInt("kass",kass);
+        editor.putInt("sälj",sälj);
+        editor.putInt("obok",obok);
+        editor.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onPostResume();
+
+        if (!resumeHasRun) {
+            resumeHasRun = true;
+            System.out.println("Wallah");
+            return;
+        }
+        SharedPreferences sp = getApplication().getSharedPreferences("SaveState", Activity.MODE_PRIVATE);
+            fullText.setText(sp.getString("fullText",null));
+        System.out.println("The Text is: " + sp.getString("fullText",null));
+            selectedTask = sp.getString("selectedTask",null);
+            startedTask = sp.getString("startedTask",null);
+            startTime = sp.getString("startTime",null);
+            started = sp.getBoolean("started",false);
+            kass = sp.getInt("kass",0);
+            sälj = sp.getInt("sälj",0);
+            obok = sp.getInt("obok",0);
+        System.out.println("Resse");
+    }
+
+
+
 
 }
